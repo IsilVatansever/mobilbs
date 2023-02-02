570fb07ded0f2af36f91dd583c1bd023feb1c1fd
Işıl <vatanseverisil@gmail.com>
2023-02-02 20:26:34 +0300

    Firebase first commit

--- app/src/main/java/com/example/myapplication/DrawerMenuActivity.java
+++ app/src/main/java/com/example/myapplication/DrawerMenuActivity.java
@@ -12,14 +12,15 @@ public class DrawerMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_menu);
+
+
         String[] menu = {"Grup Oluştur", "Gruba Üye Ekle", "Mesaj Oluştur", "Mesaj Gönder", "Logout"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menu);
 
         ListView navList = findViewById(R.id.nav_list);
         navList.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, DrawerMenuActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(MainActivity.this, DrawerActivity.class);
+        //startActivity(intent);
+        FirebaseApp.initializeApp(this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        String userid = myRef.push().getKey();
+        myRef.child(userid).setValue("deneme");
+       /* myRef.setValue("Hello, World!");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });*/
+
     }
+
 }
